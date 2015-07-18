5ca0e005a751e2814735fe8a2912f0c6d606b997
James Taylor <ChaosCloud@Jamess-iMac.local>
2015-07-18 14:29:40 -0700

    coins now disappear when collected

---
+++ Assets/Scripts/coin_disappear.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coin_disappear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+  public void  OnTriggerEnter2D(Collider2D other){
+    if(other.gameObject.tag == "family_dot"){
+      Debug.Log("family dot hit coin");
+      gameObject.SetActive(false);
+    } 
+  }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
