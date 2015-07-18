70dcf660fd85e37457466aaea74f2a5a05da70e4
James Taylor <ChaosCloud@Jamess-iMac.local>
2015-07-18 13:46:23 -0700

    first commit from james

---
+++ Assets/Scripts/Set_Collections.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Set_Collections : MonoBehaviour {
+
+  public GameObject myCoinPrefab;
+  public GameObject myLine;
+  private int totalCoins = 20;
+  public List<GameObject> family_lines;
+
+	// Use this for initialization
+	void Start () {
+    foreach(GameObject ln in family_lines){
+    
+      for(int i=-3;i<totalCoins; i++){
+        int rand = Random.Range(2,5); 
+        GameObject coin = Instantiate(myCoinPrefab, new Vector2(i*rand, ln.transform.position.y),          Quaternion.identity) as GameObject;
+      }  
+    }      	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
